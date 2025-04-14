7f991b67ad5f9abb0541c06688eedc21bf13e1de
ndavarpanah <79424197+ndavarpanah@users.noreply.github.com>
2025-04-14 16:43:24 -0700

    Better commenting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash  -e means exit script if an error occurs
 set -e
 
+#Verify it is running in Github by checking enviornment variable exists
 if [ -n "$GITHUB_EVENT_PATH" ];
     echo "Hello World!"
 fi
